8733e40ac02354ba981dbc4c3dd445d3a7f4c16c
loyti <brice@loyti.com>
2017-06-24 15:08:53 -0700

    updated js file

--- birthdaySoon.js
+++ birthdaySoon.js
@@ -2,9 +2,9 @@ var daysUntilMyBirthday = 60;
 function countDown(){
     for (var i = daysUntilMyBirthday; i >= 0; i--){
         if (i > 1){
-            document.write("<p>You only have </p>" + i + "<p>days until your birthday</p><br>");
+            document.write("<p>You only have " + i + " days until your birthday</p><br>");
         } else if (i == 1){
-                document.write("<p>Only </p>" + i + "<p> day left...</p><br>");
+                document.write("<p>Only " + i + " day left...</p><br>");
         } else {
                 document.write("<p class=\"bday\">HAPPY BIRTHDAY<p>");
         }
